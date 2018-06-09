ec6811eef447d658ce9a5f0b244cc9d3716a3e0c
Joakim Sørensen <joasoe@gmail.com>
2018-06-09 09:02:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="pyuptimerobot",
-    version="0.0.4",
+    version="0.0.5",
     author="Joakim Sorensen",
     author_email="joasoe@gmail.com",
     description="A python module to monitor Uptime Robot monitors.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ludeeus/pyuptimerobot",
+    url="https://gitlab.com/ludeeus/pyuptimerobot",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
