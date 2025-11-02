995c4c7e24a47c52b390d324c105341fda846d5b
ludeeus <joasoe@proton.me>
2025-11-02 09:34:06 +0000

    Simplify exception handling in api_request decorator

--- pyuptimerobot/decorator.py
+++ pyuptimerobot/decorator.py
@@ -62,12 +62,9 @@ def api_request(api_path: str, method: str = "GET"):
                     f"Request timeout for '{url}'"
                 ) from None
 
-            except (
-                exceptions.UptimeRobotConnectionException,
-                exceptions.UptimeRobotAuthenticationException,
-                exceptions.UptimeRobotException,
-            ) as exception:
+            except exceptions.UptimeRobotException:
                 raise
+
             except (Exception, BaseException) as exception:
                 raise exceptions.UptimeRobotException(
                     f"Unexpected exception for '{url}' with - {exception}"
