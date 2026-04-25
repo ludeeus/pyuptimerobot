7006c5a95c9112086be6119be16fe360e0e4ae16
Simone Chemelli <simone.chemelli@gmail.com>
2026-04-25 12:28:49 +0000

    revert example changes

--- example.py
+++ example.py
@@ -6,10 +6,7 @@ import aiohttp
 
 from pyuptimerobot import UptimeRobot
 
-API_KEY = "u279802-9c317034ab9d67ffdc49ca2f"
-
-TEST_ID = 801568845
-TEST_STATUS = "pause"
+API_KEY = ""
 
 async def example():
     """Example usage of pyuptimerobot."""
@@ -19,6 +16,5 @@ async def example():
         monitors = await api.async_get_monitors()
         print("Account:", account)
         print("Monitors:", monitors)
-        await api.async_edit_monitor(monitor_id=TEST_ID, status=TEST_STATUS)
 
 asyncio.run(example())
