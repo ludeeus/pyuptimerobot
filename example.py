cf7e85e6f0b50d99653ab665420766f1dedbfef9
Joakim Sørensen <joasoe@proton.me>
2026-04-29 17:28:11 +0200

    Use env in Example to prevent accidental commits with it (#143)

--- example.py
+++ example.py
@@ -1,18 +1,20 @@
 """Example usage of pyuptimerobot."""
 
 import asyncio
+import os
 
 import aiohttp
 
 from pyuptimerobot import UptimeRobot
 
-API_KEY = ""
-
 
 async def example():
     """Example usage of pyuptimerobot."""
+    if (api_key := os.getenv("UPTIMEROBOT_API_KEY")) is None:
+        print("Please set the UPTIMEROBOT_API_KEY environment variable.")
+        return
     async with aiohttp.ClientSession() as session:
-        api = UptimeRobot(API_KEY, session)
+        api = UptimeRobot(api_key, session)
         account = await api.async_get_account_details()
         monitors = await api.async_get_monitors()
         print("Account:", account)
